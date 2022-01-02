5f80175ea02e45b606e8c9ba288d6466df015406
Mert Malaz <mertmalaz@Merts-MacBook-Air.local>
2022-01-02 17:52:10 +0300

    sketch

--- sketch.js
+++ sketch.js
@@ -1,8 +1,11 @@
 // GLOBALS
+const HEIGHT = window.innerHeight
+const WIDTH = window.innerWidth
+/*
 const HEIGHT = 1080
-const WIDTH = 1920
-var diameter = 10
-var dnaHeight = 30;
+const WIDTH = 1920*/
+var diameter = 4
+var dnaHeight = 10;
 var waveCount = 10;
 let DNAs = []
 var gui;
@@ -16,13 +19,15 @@ let dnaPalette = ["#6060FF", "#FEFE00", "#3EFB00", "#F82620", "#B45A00"];
 
 let customPalette1 = ["#f72585", "#b5179e", "#7209b7", "#560bad", "#480ca8", "#3a0ca3", "#3f37c9", "#4361ee", "#4895ef", "#4cc9f0"];
 
-let customPalette2 = ["#6060FF", "#FEFE00", "#3EFB00", "#F82620", "#B45A00"];
-let customPalette3 = ["#6060FF", "#FEFE00", "#3EFB00", "#F82620", "#B45A00"];
+let customPalette2 = ["#ef476f", "#ffd166", "#06d6a0", "#118ab2", "#073b4c"];
+let customPalette3 = ["#053c5e", "#1d3958", "#353652", "#4c334d", "#643047", "#7c2e41", "#942b3b", "#ab2836", "#c32530", "#db222a"];
 
 var colorPalette = [
     customPalette1,
     myPalette1,
     dnaPalette,
+    customPalette2,
+    customPalette3
 
 ]
 
@@ -49,21 +54,64 @@ function setup() {
 }
 
 function createScene() {
-    let scaleBy = 1;
+    let scaleBy = 5;
     let transX = 0, transY = 0;
     let rot = 0;
 
-
-
-
-    DNAs.push(new DNA(-100,
-        HEIGHT / 2,
+    // Starts with right most
+    // Right 1
+    scaleBy = 5;
+    DNAs.push(new DNA(-150,
+        HEIGHT / scaleBy,
         -90,
-        4))
+        scaleBy))
 
+    // Right 2
+    scaleBy = 0.6
+    DNAs.push(new DNA(WIDTH,
+        HEIGHT / scaleBy,
+        -90,
+        scaleBy))
+    // Right 3
+    scaleBy = 2
+    DNAs.push(new DNA(-dnaHeight * scaleBy * 2,
+        HEIGHT / scaleBy,
+        -90,
+        scaleBy))
+
+    // Starts from top
+    // top 1
+    scaleBy = 2.5
     DNAs.push(new DNA(0,
-        noise((-HEIGHT / 2 + dnaHeight * scaleBy, -dnaHeight * scaleBy)),
+        -300,
         0,
+        scaleBy))
+    // top 2
+    scaleBy = 1
+    DNAs.push(new DNA(0,
+        0,
+        0,
+        scaleBy))
+    // top 3
+
+    scaleBy = 3
+    DNAs.push(new DNA(0,
+        100 - HEIGHT / scaleBy,
+        0,
+        scaleBy))
+
+    // cross 1
+    scaleBy = 1
+    DNAs.push(new DNA(-100,
+        700,
+        -45,
+        scaleBy))
+
+    // cross 2
+    scaleBy = 1.5
+    DNAs.push(new DNA(0,
+        -600,
+        60,
         scaleBy))
 
     /* Sorting the DNA array by their scale
